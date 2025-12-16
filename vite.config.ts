1d99bb5acce80f1e674e0ece66e35960309274ab
Antun Rukavina <antunrukavina8@gmail.com>
2025-12-16 10:29:42 +0100

    Fix Vite base path for Vercel + PWAa

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  // ⬇⬇⬇ KLJUČNO ZA VERCEL + PWA
   base: '/',
-
-  // opcionalno (ali dobro)
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
 })
